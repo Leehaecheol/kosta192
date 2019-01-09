f8c481b711385e651d50ab77564aec57d6cd8272
parkwonhui <parkwonhui.dev@gmail.com>
2019-01-09 11:04:05 +0900

    w 브런치 작업

--- Hello/src/kosta/Hello.java
+++ Hello/src/kosta/Hello.java
@@ -2,10 +2,8 @@ package kosta;
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("He1111111111112312");
-	System.out.println("HE11111111111112312");
-		System.out.println("¹è°íÆÄ");
-	System.out.println("Á¹·Á");
+	System.out.println("sleep");
+	System.out.println("hungry");
 	
 }
 }
