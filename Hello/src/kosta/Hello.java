4ef4d6096e95117de898bfb9495a85d11c65293d
parkwonhui <parkwonhui.dev@gmail.com>
2019-01-09 11:22:20 +0900

    modify

--- Hello/src/kosta/Hello.java
+++ Hello/src/kosta/Hello.java
@@ -2,8 +2,11 @@ package kosta;
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("sleep");
-	System.out.println("hungry");
+	System.out.println("bbbbbbb");
+	
+	
+	System.out.println("aaaaaa");
+	
 	System.out.println("zzzzz");
 	
 }
