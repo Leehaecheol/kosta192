165044ef2ad873cce57a0835f8bcad697db0915e
KOSTA <KOSTA@DESKTOP-2NP3OQM>
2019-01-09 11:03:01 +0900

    g

--- Hello/src/kosta/Hello.java
+++ Hello/src/kosta/Hello.java
@@ -3,6 +3,6 @@ package kosta;
 public class Hello {
 public static void main(String[] args) {
 	System.out.println("He1111111111112312");
-	System.out.println("HE11111111111112312");
+	System.out.println("WowWoW2312");
 }
 }
