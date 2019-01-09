ec75808adcb178d2c9316af2e14e34669ec1dd5e
parkwonhui <parkwonhui.dev@gmail.com>
2019-01-09 10:52:29 +0900

    hello2

--- Hello/src/kosta/Hello.java
+++ Hello/src/kosta/Hello.java
@@ -2,7 +2,7 @@ package kosta;
 
 public class Hello {
 public static void main(String[] args) {
-	System.out.println("Hello");
-	System.out.println("HELLO");
+	System.out.println("Hello2312312");
+	System.out.println("HELLO12312312");
 }
 }
